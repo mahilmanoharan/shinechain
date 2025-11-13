5c1542f1aa0a0d73af041e68f514ba537f3132d4
Mahil <mahil@unc.edu>
2025-11-13 15:59:09 -0500

    add deed component first iteration?

---
+++ src/AddDeed.jsx
@@ -0,0 +1,34 @@
+import { useState} from 'react'
+import {supabase} from './supabaseClient'
+
+export default funciton AddDeed(){
+    const[desc, setDesc] =useState('')
+    
+    async function handleSubmit(e){
+        e.preventDefault()
+        if (!desc) return
+
+        const {error} = await supabase
+        .from('deeds')
+        .insert({Description:desc})
+
+        if (error){
+            console.error('Error adding deed:', error)
+        } else {
+            setDesc('')
+            alert('New link added to chain! ✨')
+        }
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className="mb-4">
+            <input 
+            value={desc} 
+            onChange = {(e) => setDesc(e.target.value)}
+            placeholder="Describe your good deed..."
+            className='border rounded p-2 w-full'
+            />
+            <button className="bg yellow-400 rounded px-4 py-2 mt-2">Add Link</button>
+        </form>
+    )
+}
