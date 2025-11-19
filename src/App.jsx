44e371724990dfb37ee115a84ba5f3a6510c972b
Mahil <mahil@unc.edu>
2025-11-18 20:38:43 -0500

    slight vibecode...

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
-import DeedFeed from './DeedFeed'
 import AddDeed from './AddDeed'
+import DeedFeed from './DeedFeed'
 
-function App(){
-  return(
-    <div className="p-4 >">
-      <h1 className="text-3xl font-bold mb-4">
-        ShineChain ✨
-      </h1>
-      <AddDeed />
-      <DeedFeed />
+function App() {
+  const [prefillId, setPrefillId] = useState(null)
+  const [refreshFlag, setRefreshFlag] = useState(0) // simple way to trigger reloads
+
+  function handleAdded(newRow) {
+    // bump refresh flag so feed reloads or you can implement more targeted updates
+    setRefreshFlag((n) => n + 1)
+    setPrefillId(null)
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">ShineChain ✨</h1>
+
+      <AddDeed onAdded={handleAdded} prefillInspiredBy={prefillId} />
+
+      {/* pass a callback so user can click "Build on this" */}
+      <DeedFeed key={refreshFlag} onSelectForInspire={setPrefillId} />
     </div>
   )
-
 }
-
 
 export default App
